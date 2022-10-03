bac2f2614b1b40bcae28d3e339001d65129a27bb
nikita <reshetnikovnikita@gmail.com>
2022-10-03 22:23:19 +0300

    целые числа

---
+++ Desktop/geek brains/SEMINAR 1/5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая при вводе N, все целые числа от -N до N 
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int N2 = N*-1;
+int index = N2;
+Console.Write(N2);
+while(  index < N)
+{
+    N2= N2 + 1;
+    Console.Write(N2);
+    index = index + 1;
+}
