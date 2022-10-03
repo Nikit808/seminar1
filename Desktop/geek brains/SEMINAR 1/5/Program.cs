8dd1047997875d5bc4b803906dfbebf1ec12f31f
nikita <reshetnikovnikita@gmail.com>
2022-10-03 23:59:22 +0300

    updated

--- Desktop/geek brains/SEMINAR 1/5/Program.cs
+++ Desktop/geek brains/SEMINAR 1/5/Program.cs
@@ -5,11 +5,10 @@ Console.Clear();
 Console.WriteLine("Введите число: ");
 int N = int.Parse(Console.ReadLine()!);
 int N2 = N*-1;
-int index = N2;
 Console.WriteLine(N2);
-while(  index < N)
+while(  N2 < N)
 {
     N2= N2 + 1;
     Console.WriteLine(N2);
-    index = index + 1;
+    
 }
