174717f95e1fe8f5959744ff9fa8d74961eda0d1
nikita <reshetnikovnikita@gmail.com>
2022-10-03 19:23:25 +0300

    программа, которая сичтает квадрат

---
+++ Desktop/geek brains/SEMINAR 1/0/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа которая считает квадрат от числа
+
+Console.Clear ();
+Console.Write (" введите число: ");
+
+int number = int.Parse (Console.ReadLine()!);
+
+int sqr = number*number; 
+Console.WriteLine($"квадрат числа {number} равен {sqr}");
