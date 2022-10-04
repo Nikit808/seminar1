37df9ed5d55a5ec5f3b5a34383d5333d7a0e2a26
nikita <reshetnikovnikita@gmail.com>
2022-10-04 17:44:27 +0300

    задача номер 4

---
+++ Desktop/geek brains/SEMINAR 1/4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2. Найти максимум из трех чисел
+
+Console.Clear();
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число d: ");
+int d = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (d > max) max = d;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
+
