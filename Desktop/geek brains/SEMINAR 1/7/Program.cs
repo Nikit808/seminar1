ee8b0460bd341edc3b34301bcde3094504c96b87
nikita <reshetnikovnikita@gmail.com>
2022-10-04 15:17:04 +0300

    updated

---
+++ Desktop/geek brains/SEMINAR 1/7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Принимает трехначное число и показывает последнюю цифру
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: "); 
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{N%10}");
+
