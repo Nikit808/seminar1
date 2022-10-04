b1cd2bb25dba07a018f16c9d89dd380c7b42f54e
nikita <reshetnikovnikita@gmail.com>
2022-10-04 18:19:34 +0300

    задача номер 8

---
+++ Desktop/geek brains/SEMINAR 1/8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int N2 = 2;
+Console.Write("Четные Числа: ");
+while (N2 <= N)
+{
+    Console.Write($"{N2} ");
+    N2 = N2 + 2; 
+}
