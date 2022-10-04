d4c810d9d6f1bd30440153a4b2bee953777d4ffc
nikita <reshetnikovnikita@gmail.com>
2022-10-04 15:52:02 +0300

    задача номер 2

---
+++ Desktop/geek brains/SEMINAR 1/2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2. Найти максимум из двух чисел
+
+Console.Clear();
+
+Console.WriteLine("Введите число a: "); 
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число b: "); 
+int b = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+    Console.Write($"max={a}, min={b}");
+}
+else
+{
+    Console.Write($"max={b}, min={a}");
+}
