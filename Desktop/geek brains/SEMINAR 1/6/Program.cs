19b58b9ca2950c5e6585a29291b44058380eb0c7
nikita <reshetnikovnikita@gmail.com>
2022-10-04 18:05:25 +0300

    задача номер 6

---
+++ Desktop/geek brains/SEMINAR 1/6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6. Найти четное 
+
+Console.Clear();
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2==0)
+Console.WriteLine($"Число {a} - Четное"); 
+else
+Console.WriteLine($"Число {a} - Нечетное");
+
