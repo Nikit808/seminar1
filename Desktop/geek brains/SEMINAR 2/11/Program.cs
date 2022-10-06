0a75f771f13994021d11fe5eabb37c5df5fc2f29
nikita <reshetnikovnikita@gmail.com>
2022-10-06 21:50:29 +0300

    задача 11

---
+++ Desktop/geek brains/SEMINAR 2/11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программма выводит случайное трех значное число и убирает вторую цифру
+
+Console.Clear ();
+int num = new Random().Next (100, 1000);
+
+int a1 = num/100;
+int a3 = num%10;
+
+Console.Write ($"Число {num} - {a1}{a3}");
+
+
+
