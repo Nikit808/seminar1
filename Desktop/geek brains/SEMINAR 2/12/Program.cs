7613d4ea2eff03941e2ec0484a37a909abe60097
nikita <reshetnikovnikita@gmail.com>
2022-10-07 20:19:07 +0300

    задача 12

---
+++ Desktop/geek brains/SEMINAR 2/12/Program.cs
@@ -0,0 +1,19 @@
+﻿// задача про кратность 
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a % b == 0)
+{
+    Console.Write("Кратно");
+}
+else
+{
+    Console.Write($"Не кратно, остаток {a % b}");
+}
+
+
