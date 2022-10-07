cd70f1cdaa09d3feb6c159a9609a1f813dfb548b
nikita <reshetnikovnikita@gmail.com>
2022-10-07 20:36:53 +0300

    задача 14

---
+++ Desktop/geek brains/SEMINAR 2/14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа проверяет краность на 7 и 23
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%7==0 && a%23==0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
