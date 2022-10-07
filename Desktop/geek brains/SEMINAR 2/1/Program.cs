0f8f8b9b11d20ce26aa2f31b51ff728b13f09779
nikita <reshetnikovnikita@gmail.com>
2022-10-07 20:20:40 +0300

    ignore

--- Desktop/geek brains/SEMINAR 2/1/Program.cs
+++ Desktop/geek brains/SEMINAR 2/1/Program.cs
@@ -4,5 +4,13 @@
 
 Console.Clear ();
 int num = new Random().Next (10, 100);
+int a1 = num/10;
+int a2 = num%10; 
 
+int max=a1;
+if (max<a2)
+{
+    max=a2;
+}
+Console.Write($"Макс цифра числа {num} - {max}");
 
