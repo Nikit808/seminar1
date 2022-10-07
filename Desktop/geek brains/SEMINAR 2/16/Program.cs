e2d80fd8d3ae4ae19ae5553307ecb00d407d81a0
nikita <reshetnikovnikita@gmail.com>
2022-10-07 21:21:34 +0300

    задача 16

---
+++ Desktop/geek brains/SEMINAR 2/16/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа проверяет является ли число 1 квадратом второго
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a*a==b||b*b==a)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
