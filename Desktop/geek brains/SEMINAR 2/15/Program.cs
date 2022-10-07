3cb49f8b6c7999cc8bbfce0528272fbdb28edecf
nikita <reshetnikovnikita@gmail.com>
2022-10-07 22:14:12 +0300

    задача 15

---
+++ Desktop/geek brains/SEMINAR 2/15/Program.cs
@@ -0,0 +1,18 @@
+﻿// / Напишите программу, которая будет выдавать назвние дня недели
+
+Console.Clear();
+Console.WriteLine("Введите число дня недели: ");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x ==1||x==2||x==3||x==4||x==5)
+{
+    Console.WriteLine("нет");
+}
+else if (x == 6||x==7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Введите число дня недели снова: ");
+}
