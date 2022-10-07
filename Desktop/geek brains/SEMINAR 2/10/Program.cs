ebe234d103465f336b8ddd7869d7514f32de1525
nikita <reshetnikovnikita@gmail.com>
2022-10-07 22:03:09 +0300

    задача 10

---
+++ Desktop/geek brains/SEMINAR 2/10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ввестти трехзначное число и на выходе показать вторую цифру
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначно число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a2=num/10%10;
+
+Console.Write ($"Число {num} - {a2}");
+
+
+
