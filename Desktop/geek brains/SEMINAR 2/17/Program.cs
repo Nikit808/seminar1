31501b05ed2be867f9e853ce5aa8682946080a73
nikita <reshetnikovnikita@gmail.com>
2022-10-14 18:02:41 +0300

    задача 17

---
+++ Desktop/geek brains/SEMINAR 2/17/Program.cs
@@ -0,0 +1,31 @@
+﻿// 17 задача с четвертями
+
+Console.Clear();
+Console.Write("Введите Х: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+}
+else
+{
+    if (y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else
+{
+    Console.WriteLine("Третья четверть");
+}
+}
