5adb5cd51f62d4cf1e6b07efb65b92d6c167e423
nikita <reshetnikovnikita@gmail.com>
2022-10-20 20:27:15 +0300

    задача 27

---
+++ Desktop/geek brains/SEMINAR 2/27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int a)
+{
+    int Sum = 0;
+    while (a != 0)
+    {
+        Sum =Sum + a%10;
+        a=a/10;
+    }
+return Sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(n));
