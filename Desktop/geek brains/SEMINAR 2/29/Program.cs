c49d65b2f374a9625fe4236855917ba6009eb043
nikita <reshetnikovnikita@gmail.com>
2022-10-21 19:28:36 +0300

    задача 29

---
+++ Desktop/geek brains/SEMINAR 2/29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,100);
+        index++;
+    }
+}
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+    if (i != mass.Length - 1)
+        {
+        Console.Write(", ");
+        }
+    }
+}
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
