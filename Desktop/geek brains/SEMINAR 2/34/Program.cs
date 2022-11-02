b1e8531c7abf247fce3635108d6d5e7da8a7ec7f
nikita <reshetnikovnikita@gmail.com>
2022-11-02 23:05:53 +0300

    задача 34

---
+++ Desktop/geek brains/SEMINAR 2/34/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+            res[i] = new Random().Next(100, 1000);
+    }
+return res;
+}
+
+int GetCount(int[] numArray)
+    {
+        int count = 0;
+    for (int i = 0; i < numArray.Length; i++)
+        {
+            if (numArray[i] %2==0)
+            {
+                count++;
+            }
+        }
+    return count;
+    }
+Console.Clear();
+
+int[] array = GetArray(4, 100, 1000);
+Console.WriteLine(String.Join(",", array));
+Console.Write($"Количество четных чисел: {GetCount(array)}");
