ad5b28f4b59de51967b01881f096eba13ea9827f
nikita <reshetnikovnikita@gmail.com>
2022-11-07 12:43:11 +0300

    задача 41

---
+++ Desktop/geek brains/SEMINAR 2/41/Program.cs
@@ -0,0 +1,32 @@
+﻿// 41
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+    res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int Count(int[] numArray)
+    {
+        int count = 0;
+    for (int i = 0; i < numArray.Length; i++)
+        {
+            if (numArray[i]>0)
+            {
+                count++;
+            }
+        }
+    return count;
+    }
+Console.Clear();
+
+Console.WriteLine("Введите числа "); 
+String input=Console.ReadLine()!;
+int [] numArray=GetArrayFromString(input);
+int count=Count(numArray); 
+Console.WriteLine($"Кол-во положительных чисел: {count}");
