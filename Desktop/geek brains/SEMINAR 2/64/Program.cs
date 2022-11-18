cf5c8c0a36488e1a1c14891d09c45f483b5018d0
nikita <reshetnikovnikita@gmail.com>
2022-11-18 13:27:36 +0300

    задача 64

---
+++ Desktop/geek brains/SEMINAR 2/64/Program.cs
@@ -0,0 +1,20 @@
+﻿// 64
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Numbers(number, count);
+
+
+void Numbers(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        Numbers(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
