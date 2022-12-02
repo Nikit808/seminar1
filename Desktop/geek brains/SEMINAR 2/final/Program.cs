724d27e4d4d3a0f44a89e6446a56329bf4cb968b
nikita <reshetnikovnikita@gmail.com>
2022-12-02 14:07:51 +0300

    начало

---
+++ Desktop/geek brains/SEMINAR 2/final/Program.cs
@@ -0,0 +1,25 @@
+﻿// 
+string[] arrayIN = new string[] {"1234", "1567", "-2", "computer science"};
+string[] arrayOUT = new string[arrayIN.Length];
+void ArrayINandOUT(string[] arrayIN, string[] arrayOUT)
+{
+    int count = 0;
+    for (int i = 0; i < arrayIN.Length; i++)
+    {
+        if (arrayIN[i].Length <= 3)
+        {
+            arrayOUT[count] = arrayIN[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayINandOUT(arrayIN, arrayOUT);
+PrintArray(arrayOUT);
